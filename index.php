04a3fe85a456bb5510b5b4f30f5b10394159f4f9
DeVinchi21 <86185422+DeVinchi21@users.noreply.github.com>
2021-06-20 15:21:55 +0300

    Update index.php

--- index.php
+++ index.php
@@ -13,6 +13,13 @@
 
     echo "<h1>Welcome ro ". $name."</h1>";
     echo "<h2> You have $".$credit."</h2>";
+
+        $products['Computer']=750;
+        $products['Car']=15000;
+        $products['iPhone']=1000;
+        $products['Toaster']=75;
+
+        echo "<p> A car costs $".$products['Car']."</p>";
     ?>
 </body>
 </html>
